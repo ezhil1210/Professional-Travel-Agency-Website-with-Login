cf8ad45d179ea13f8cb0850b0d9fea54cac769fc
ezhil1210 <105728269+ezhil1210@users.noreply.github.com>
2023-08-14 20:49:34 +0530

    Travel Agency Website with signup/Login created using html, css, php and javascript in main code and phpmyadmin web server for database. Fully functional Professional Website with clean and modern UI.
    Travel Agency Website with signup/Login created using html, css, php and javascript in main code and phpmyadmin web server for database. Fully functional Professional Website with clean and modern UI.

---
+++ Project Final/index.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sign Up</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<style media="screen">
+      *,
+*:before,
+*:after{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins',sans-serif;
+}
+body{
+    background-color: #080710;
+}
+.background{
+    width: 430px;
+    height: 520px;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    left: 50%;
+    top: 50%;
+}
+.background .shape{
+    height: 200px;
+    width: 200px;
+    position: absolute;
+    border-radius: 50%;
+}
+.shape:first-child{
+    background: linear-gradient(
+        #1845ad,
+        #23a2f6
+    );
+    left: -80px;
+    top: -80px;
+}
+.shape:last-child{
+    background: linear-gradient(
+        to right,
+        #ff512f,
+        #f09819
+    );
+    right: -30px;
+    bottom: -80px;
+}
+form{
+    height: 580px;
+    width: 400px;
+    background-color: rgba(255,255,255,0.13);
+    position: absolute;
+    transform: translate(-50%,-50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 10px;
+    backdrop-filter: blur(10px);
+    border: 2px solid rgba(255,255,255,0.1);
+    box-shadow: 0 0 40px rgba(8,7,16,0.6);
+    padding: 50px 35px;
+}
+form *{
+    font-family: 'Poppins',sans-serif;
+    color: #ffffff;
+    letter-spacing: 0.5px;
+    outline: none;
+    border: none;
+}
+form.h3{
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 42px;
+    text-align: center;
+}
+
+label{
+    display: block;
+    margin-top: 30px;
+    font-size: 16px;
+    font-weight: 500;
+}
+input{
+    display: block;
+    height: 50px;
+    width: 100%;
+    background-color: rgba(255,255,255,0.07);
+    border-radius: 3px;
+    padding: 0 10px;
+    margin-top: 8px;
+    font-size: 14px;
+    font-weight: 300;
+}
+::placeholder{
+    color: #e5e5e5;
+}
+button{
+    margin-top: 50px;
+    width: 100%;
+    background-color: #ffffff;
+    color: #080710;
+    padding: 15px 0;
+    font-size: 18px;
+    font-weight: 600;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.social{
+  margin-top: 30px;
+  display: flex;
+}
+.social div{
+  background: red;
+  width: 150px;
+  border-radius: 3px;
+  padding: 5px 10px 10px 5px;
+  background-color: rgba(255,255,255,0.27);
+  color: #eaf0fb;
+  text-align: center;
+}
+.social div:hover{
+  background-color: rgba(255,255,255,0.47);
+}
+.social .fb{
+  margin-left: 25px;
+}
+.social i{
+  margin-right: 4px;
+}
+h4.display-4{
+  font-size: 30px;
+    font-weight: 500;
+    line-height: 10px;
+    text-align: center;
+    vertical-align:center;
+    padding: 20px 0px 0px 0px;
+
+}
+p.textforlogin{
+  text-align:left;
+}
+
+    </style>
+<body>
+<div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+    	
+    	<form class="shadow w-450 p-3" 
+    	      action="php/signup.php" 
+    	      method="post">
+
+    		<h4 class="display-4  fs-1">Create Account</h4><br>
+    		<?php if(isset($_GET['error'])){ ?>
+    		<div class="alert alert-danger" role="alert">
+			  <?php echo $_GET['error']; ?>
+			</div>
+		    <?php } ?>
+
+		    <?php if(isset($_GET['success'])){ ?>
+    		<div class="alert alert-success" role="alert">
+			  <?php echo $_GET['success']; ?>
+			</div>
+		    <?php } ?>
+		  <div class="mb-3">
+		    <label class="form-label">Full Name</label>
+		    <input type="text" 
+		           class="form-control"
+		           name="fname"
+		           value="<?php echo (isset($_GET['fname']))?$_GET['fname']:"" ?>">
+		  </div>
+
+		  <div class="mb-3">
+		    <label class="form-label">User name</label>
+		    <input type="text" 
+		           class="form-control"
+		           name="uname"
+		           value="<?php echo (isset($_GET['uname']))?$_GET['uname']:"" ?>">
+		  </div>
+
+		  <div class="mb-3">
+		    <label class="form-label">Password</label>
+		    <input type="password" 
+		           class="form-control"
+		           name="pass">
+		  </div>
+		  
+		  <button type="submit" class="btn btn-primary">Sign Up</button><br></br>
+		  <p class="textforlogin">Already have an Account!!<a href="login.php" class="link-secondary">Login  Here</a>
+		</form>
+    </div>
+</body>
+</html>
